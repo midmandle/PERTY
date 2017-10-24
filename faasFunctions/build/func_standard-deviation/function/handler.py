a59ee0916948bbf9ddd07140528a506b5cb77d05
Alasdair Smith <alasdairsmith0491@gmail.com>
2017-10-24 16:32:16 +0100

    Implemented the basics for FaaS containers using OpenFaaS.

---
+++ faasFunctions/build/func_standard-deviation/function/handler.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def handle(req):
+    json_req = json.loads(req)
+    estimate = json_req["estimate"]
+    optimistic = json_req["optimistic"]
+    pessimistic = json_req["pessimistic"]
+
+    stdDev = (pessimistic - optimistic) / 6.0
+    stdDev = round(stdDev, 2)
+    results = {"standardDeviation" : stdDev}
+
+    print json.dumps(results)
