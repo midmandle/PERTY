89d1d0e5260132230aabd5819e2c94cb3d6c23b5
RootDevil <jonathon.coolledge@gmail.com>
2017-10-24 13:00:03 +0100

    Update UnitTest_pert.py

--- python/UnitTest_pert.py
+++ python/UnitTest_pert.py
@@ -1,97 +1,41 @@
 import unittest
 from task import Task
-from tasklist import TaskList
 
 class pertEquationTests(unittest.TestCase):
+
+    def setUp(self):
+        self.estimate = 3.0
+        self.optimistic = 1.0
+        self.pessimistic = 12.0
+
+        self.taskA = Task(self.optimistic, self.estimate, self.pessimistic, 3)
+
+    def tearDown(self):
+        self.estimate = 0
+        self.optimistic = 0
+        self.pessimistic = 0
+        self.taskA = None
+
     def test_expectedDurationCalculation(self):
-        estimate = 3.0
-        optimistic = 1.0
-        pessimistic = 12.0
-
-        taskA = Task(optimistic, estimate, pessimistic, 3)
-
-        self.assertEqual(4.17, taskA.expected)
+        self.assertEqual(4.17, self.taskA.expected)
 
     def test_standardDeviationCalculation(self):
-        estimate = 3.0
-        optimistic = 1.0
-        pessimistic = 12.0
-
-        taskA = Task(optimistic, estimate, pessimistic, 3)
-
-        self.assertEqual(1.83, taskA.stdDev)
+        self.assertEqual(1.83, self.taskA.stdDev)
 
     def test_bestCaseEstimate(self):
-        estimate = 3.0
-        optimistic = 1.0
-        pessimistic = 12.0
-
-        taskA = Task(optimistic, estimate, pessimistic, 3)
-
-        bestCaseEstimate_value = taskA.expected - (taskA.multiplier * taskA.stdDev)
-
-        self.assertEqual(bestCaseEstimate_value, taskA.bestCase)
+        bestCaseEstimate_value = self.taskA.expected - (self.taskA.multiplier * self.taskA.stdDev)
+        self.assertEqual(bestCaseEstimate_value, self.taskA.bestCase)
 
     def test_worstCaseEstimate(self):
-        estimate = 3.0
-        optimistic = 1.0
-        pessimistic = 12.0
-
-        taskA = Task(optimistic, estimate, pessimistic, 3)
-
-        worstCaseEstimate_value = taskA.expected + (taskA.multiplier * taskA.stdDev)
-
-        self.assertEqual(worstCaseEstimate_value, taskA.worstCase)
+        worstCaseEstimate_value = self.taskA.expected + (self.taskA.multiplier * self.taskA.stdDev)
+        self.assertEqual(worstCaseEstimate_value, self.taskA.worstCase)
 
     def test_priorityCalculation(self):
-        estimate = 3.0
-        optimistic = 1.0
-        pessimistic = 12.0
+        bestCasePriority = self.taskA.weight/self.taskA.bestCase
+        self.assertEqual(bestCasePriority, self.taskA.priorityBest)
 
-        taskA = Task(optimistic, estimate, pessimistic, 3)
+        expectedDurationPriority = self.taskA.weight/self.taskA.expected
+        self.assertEqual(expectedDurationPriority, self.taskA.priorityExpected)
 
-        bestCasePriority = taskA.weight/taskA.bestCase
-        self.assertEqual(bestCasePriority, taskA.priorityBest)
-
-        expectedDurationPriority = taskA.weight/taskA.expected
-        self.assertEqual(expectedDurationPriority, taskA.priorityExpected)
-
-        worstCasePriority = taskA.weight/taskA.worstCase
-        self.assertEqual(worstCasePriority, taskA.priorityWorst)
-
-    def test_addTaskToTaskList(self):
-        estimate = 3.0
-        optimistic = 1.0
-        pessimistic = 12.0
-
-        taskA = Task(optimistic, estimate, pessimistic, 3)
-        taskB = Task(1.0, 1.5, 14.0, 4)
-
-        taskList = TaskList();
-        self.assertEqual(taskList.taskCount, 0) #Originally no items.
-        taskList.addTask(taskA)
-        taskList.addTask(taskB)
-        self.assertEqual(taskList.taskCount, 2) #Now two should exist.
-
-    def test_removeTaskFromTaskList(self):
-        estimate = 3.0
-        optimistic = 1.0
-        pessimistic = 12.0
-
-        taskA = Task(optimistic, estimate, pessimistic, 3)
-        taskB = Task(1.0, 1.5, 14.0, 4)
-
-        taskList = TaskList();
-        self.assertEqual(taskList.taskCount, 0) #Originally no items.
-        taskList.addTask(taskA)
-        taskList.addTask(taskB)
-        self.assertEqual(taskList.taskCount, 2) #Now two should exist.
-
-        taskList.removeTask(taskA)
-        self.assertEqual(taskList.taskCount, 1)
-        taskList.removeTask(taskB)
-        self.assertEqual(taskList.taskCount, 0)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        worstCasePriority = self.taskA.weight/self.taskA.worstCase
+        self.assertEqual(worstCasePriority, self.taskA.priorityWorst)
